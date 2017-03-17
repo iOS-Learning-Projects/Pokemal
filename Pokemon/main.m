95d84c1442015cf6cf2c3396dd52cdb91f023735
Bruno Chagas <brunochagas@Mac305.local>
2017-03-17 11:34:43 -0300

    Initial Commit

---
+++ Pokemon/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pokemon
+//
+//  Created by Bruno Chagas on 17/03/17.
+//  Copyright © 2017 Bruno Chagas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
